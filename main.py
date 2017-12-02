2a9bd3263fdb9fd5a63a4ac8531faefad1af4040
Chrysostomos Marasinou <cmarasinou@Chrysostomoss-MacBook-Pro.local>
2017-12-02 10:22:59 -0800

    Added form

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # Create an app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 # Initialize bootstrap
 bootstrap = Bootstrap(app)
+
+# Class of the name form
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators = [DataRequired()])
+    submit = SubmitField('Submit')
 
 # Home route
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/<name>')
+# User route
+@app.route('/user/<name>')
 def user(name):
     return render_template("user.html", name = name)
+
+# Form route
+@app.route('/form', methods = ["GET", "POST"])
+def nameform():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template("nameform.html", name = name, form = form)
 
 # Run the app
 if __name__ == '__main__':
